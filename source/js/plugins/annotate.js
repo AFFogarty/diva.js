bd33c4ee3558fdd352103a284783f55de1745323
AFFogarty <andrew.f.fogarty@gmail.com>
2014-07-30 15:01:28 -0400

    New: Full-page annotations

--- source/js/plugins/annotate.js
+++ source/js/plugins/annotate.js
@@ -12,10 +12,53 @@ Allows you to highlight regions of a page image
         {
             init: function(divaSettings, divaInstance)
             {
-                // initialize an empty highlights object.
-                divaSettings.parentSelector.data('annotations', {});
+                // Initialize an empty highlights object.
+                divaSettings.parentSelector.data(
+                        'annotations',
+                        {
+                            page: {},
+                            area: {}
+                        });
+                var annotationObj = divaSettings.parentSelector.data('annotations');
+                
+                // Event handlers
+                diva.Events.subscribe("VisiblePageDidChange", _page_change_event_handler);  
+                
+                /*
+                 * Private functions
+                 */
+                
+                /**
+                 * Callback function fired when the page changes.
+                 * 
+                 * @param {type} pageIdx
+                 * @returns {undefined}
+                 */
+                function _page_change_event_handler(pageIdx, pageName)
+                {
+                    if (annotationObj.page[pageIdx])
+                    {
+                        // Print it out to the console.
+                        diva.Events.publish("PageAnnotation", [annotationObj.page[pageIdx]]);
+                    }
+                }
+                
 
 
+                /*
+                 * Public functions
+                 */
+                
+                divaInstance.annotatePage = function(pageIdx, textContent)
+                {
+                    // Save it
+                    annotationObj.page[pageIdx] = String(textContent);
+                };
+                divaInstance.getAnnotations = function()
+                {
+                   return annotationObj;
+                };
+                
                 return true;
             },
             pluginName: 'annotate',
