852b499b2dfacce0c66bf52ae552b55879115b72
AFFogarty <andrew.f.fogarty@gmail.com>
2014-07-31 12:05:25 -0400

    New: Bookmark localStorage data saves and can be retrieved

--- source/js/plugins/bookmark.js
+++ source/js/plugins/bookmark.js
@@ -13,18 +13,35 @@ Bookmark locations in the document.
             {
                 // Check if the browser can do local storage
                 if (typeof(Storage) !== "undefined") {
-                    if (localStorage.getItem("diva-bookmarks") === undefined)
+                    if (localStorage.getItem("diva-bookmarks") === null)
                     {
                         // Set the empty bookmark list
-                        localStorage.setItem("diva-bookmarks", []);
+                        localStorage.setItem("diva-bookmarks", JSON.stringify([]));
                     }
                     // Grab the list
-                    var bookmarkObject = localStorage.getItem("diva-bookmarks");
+                    var bookmarkObject = JSON.parse(localStorage.getItem("diva-bookmarks"));
+                    console.log("BookmarkObject: ");
+                    console.log(bookmarkObject);
                 } else {
                     // User's browser doesn't support local storage
+                    console.log("Browser does not support local storage.");
                     return true;
                 }
 
+                function _add_bookmark(pageIndex)
+                {
+                    bookmarkObject.push(parseInt(pageIndex));
+                }
+
+                /**
+                 * Sort the bookmark list by page.
+                 *
+                 * @private
+                 */
+                function _sort_bookmarks()
+                {
+                    // TODO
+                }
 
                 /**
                  * Persist the bookmarks to the user's browser.
@@ -33,8 +50,20 @@ Bookmark locations in the document.
                  */
                 function _save_bookmarks()
                 {
-                    localStorage.setItem("diva-bookmarks", bookmarkObject);
+                    localStorage.setItem("diva-bookmarks",
+                        JSON.stringify(bookmarkObject));
                 }
+
+                divaInstance.bookmarkCurrentLocation = function()
+                {
+                    _add_bookmark(5);
+                    _save_bookmarks();
+                };
+
+                divaInstance.getBookmarks = function()
+                {
+                    return bookmarkObject;
+                };
 
                 return true;
             },
