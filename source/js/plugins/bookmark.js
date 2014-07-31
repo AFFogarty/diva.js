ba065746e7f10f7e6e04e6f84acad4fee4475a37
AFFogarty <andrew.f.fogarty@gmail.com>
2014-07-31 11:43:13 -0400

    New: Initialize the bookmark storage

--- source/js/plugins/bookmark.js
+++ source/js/plugins/bookmark.js
@@ -11,8 +11,20 @@ Bookmark locations in the document.
         {
             init: function(divaSettings, divaInstance)
             {
+                // Check if the browser can do local storage
+                if (typeof(Storage) !== "undefined") {
+                    if (localStorage.getItem("diva-bookmarks") === undefined)
+                    {
+                        // Set the empty bookmark list
+                        localStorage.setItem("diva-bookmarks", []);
+                    }
+                    // Grab the list
+                    var bookmarkObject = localStorage.getItem("diva-bookmarks");
+                } else {
+                    // User's browser doesn't support local storage
+                    return true;
+                }
 
-                
                 return true;
             },
             pluginName: 'bookmark',
