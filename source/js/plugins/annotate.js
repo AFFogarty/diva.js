a9934212427323ff3555559349cad67f4425e627
AFFogarty <andrew.f.fogarty@gmail.com>
2014-08-04 17:31:01 -0400

    New:  Some progress on proper annotations

--- source/js/plugins/annotate.js
+++ source/js/plugins/annotate.js
@@ -5,6 +5,24 @@ Allows you to highlight regions of a page image
 
 (function ($)
 {
+    "use strict";
+
+    /**
+     * A UUID helper function that I found online.
+     */
+    var guid = (function() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return function() {
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        };
+    })();
+
+
     window.divaPlugins.push((function()
     {
         var settings = {};
@@ -12,50 +30,176 @@ Allows you to highlight regions of a page image
         {
             init: function(divaSettings, divaInstance)
             {
-                // Initialize an empty highlights object.
-                divaSettings.parentSelector.data(
-                        'annotations',
-                        {
-                            page: {},
-                            area: {}
-                        });
-                var annotationObj = divaSettings.parentSelector.data('annotations');
-                
+
+                // Create the pop-up window.
+                $(divaSettings.parentSelector).append('<div class="diva-annotate-window">TEST!</div>');
+                var annotationsDiv = $(".diva-annotate-window");
+                _render_annotate_window();
+
+                var annotationObj = {};
+
                 // Event handlers
-                diva.Events.subscribe("VisiblePageDidChange", _page_change_event_handler); 
+                diva.Events.subscribe("VisiblePageDidChange", _page_change_event_handler);
                 diva.Events.subscribe("HighlightCompleted", _prepare_clickable_annotations);
-                
+
+                /*
+                Classes
+                 */
+
+                var Annotation = (function () {
+                    function Annotation(x, y) {
+                        /*
+                        Class fields
+                         */
+                        this.text =  "";
+                        // Whether or not the window is opened.
+                        this.isOpen = false;
+                        // Location
+                        this.x = 0;
+                        this.y = 0;
+                        this.noteDiv = null;
+                        this.editWindow = null;
+
+                        // A UUID for identifying the note
+                        this.uuid = guid();
+
+                        /*
+                        Construction
+                         */
+                        this.setX(x);
+                        this.setY(y);
+
+                        // Create the note div
+                        $(divaSettings.parentSelector).append('<div class="annotation ' + this.uuid + '"></div>');
+                        // Pick out the note div so that we can keep track
+                        this.noteDiv = divaSettings.parentSelector.find("." + this.uuid);
+                        console.log(this.noteDiv);
+                        // Open on click
+                        this.noteDiv.click(this.open);
+                    }
+
+                    /**
+                     * Set the X location of the note.
+                     *
+                     * @param pX
+                     */
+                    Annotation.prototype.setX = function (pX) {
+                        this.x = parseInt(pX);
+                    };
+
+                    /**
+                     * Set the Y location of the note.
+                     *
+                     * @param pY
+                     */
+                    Annotation.prototype.setY = function (pY) {
+                        this.y = parseInt(pY);
+                    };
+
+                    /**
+                     * Open the edit window for the given note.
+                     */
+                    Annotation.prototype.open = function () {
+                        console.log(this.isOpen);
+                        if (this.isOpen === false) {
+                            // Create the edit window
+                            $(divaSettings.parentSelector).append(
+                                '<div class="' + this.uuid + '">Test</div>');
+                            this.editWindow = $(divaSettings.parentSelector + " ");
+                            this.isOpen = true;
+                        }
+                    };
+
+                    /**
+                     * Close the edit window
+                     */
+                    Annotation.prototype.close = function () {
+                        if (this.isOpen)
+                        {
+                            this.editWindow.remove();
+                            this.isOpen = false;
+                        }
+                    };
+
+                    Annotation.prototype.renderNote = function () {
+                        this.noteDiv.setX(this.x);
+                        this.noteDiv.setY(this.y);
+                    };
+                    return Annotation;
+                })();
+
+
                 /*
                  * Private functions
                  */
-                
+
                 /**
                  * Callback function fired when the page changes.
-                 * 
-                 * @param {type} pageIdx
-                 * @returns {undefined}
+                 *
+                 * @param pageIdx
+                 * @param pageName
+                 * @private
                  */
                 function _page_change_event_handler(pageIdx, pageName)
                 {
                     // Save the current page
-                    currentPage = pageIdx;
-                    // Point to the correct 
-                    
-                    if (annotationObj.page[pageIdx])
-                    {
-                        // Print it out to the console.
-                        diva.Events.publish("PageAnnotation", [annotationObj.page[pageIdx]]);
-                        
-                    }
-                }
-                
-                function _area_click_event_handler()
-                {
-                    // TODO
+                    var currentPage = pageIdx;
+                    // Point to the correct
+
+//                    if (annotationObj.page[pageIdx])
+//                    {
+//                        // Print it out to the console.
+//                        diva.Events.publish("PageAnnotation", [annotationObj.page[pageIdx]]);
+//
+//                    }
                 }
 
+
                 /**
-                 * 
+                 * Render the entire bookmarks window.
+                 *
+                 * @private
+                 */
+                function _render_annotate_window()
+                {
+                    // So that we don't have memory leaks
+                    annotationsDiv.empty();
+
+                    var content = '<div class="diva-annotate-window-form"><div class="diva-annotate-window-toolbar">' +
+                        '<div class="diva-annotate-window-close" ' +
+                        'title="Close the bookmarks window"></div></div>';
+
+                    content += '<h3>Create Annotation</h3> <form class="create-annotation">' +
+                        '<textarea rows="4" cols="35" class="annotation-name" placeholder="Name"></textarea>' +
+                        '<input type="submit" value="Create"></form></div>';
+
+                    // Fill it with the content
+                    annotationsDiv.html(content);
+
+                    annotationsDiv.find(".create-annotation").each(
+                        function()
+                        {
+                            $(this).submit(
+                                function(event)
+                                {
+                                    event.preventDefault();
+                                    var name = annotationsDiv.find(".annotation-name").val();
+                                    divaInstance.bookmarkCurrentLocation(name);
+                                }
+                            );
+                        }
+                    );
+                    annotationsDiv.find(".diva-annotate-window-close").click(
+                        function()
+                        {
+                            annotationsDiv.hide();
+                        }
+                    );
+                }
+
+
+                /**
+                 *
                  * @param {type} pageIdx
                  * @param {type} filename
                  * @param {type} pageSelector
@@ -64,16 +208,14 @@ Allows you to highlight regions of a page image
                 function _annotate_area(pageIdx, filename, pageSelector)
                 {
                     if (typeof annotationObj === 'undefined')
-                        return;
-                    
-                    if (highlightObj.hasOwnProperty(pageIdx))
                     {
-                        // Create a box to represent the area
-                        
+                        return;
                     }
-                    diva.Events.publish("AnnotateAreaCompleted");                    
-                };
-                
+
+
+                    diva.Events.publish("AnnotateAreaCompleted");
+                }
+
                 function _prepare_clickable_annotations()
                 {
                     // Get all of currently visible pages
@@ -87,7 +229,10 @@ Allows you to highlight regions of a page image
                         {
                             visiblePages.push(i);
                         }
-                        else break;
+                        else
+                        {
+                            break;
+                        }
                     }
                     // Pages below the current
                     for (i = currentPage - 1; i >= 0; i--)
@@ -96,34 +241,42 @@ Allows you to highlight regions of a page image
                         {
                             visiblePages.push(i);
                         }
-                        else break;
-                    }    
+                        else
+                        {
+                            break;
+                        }
+                    }
                     visiblePages = visiblePages.sort(function(a,b){return a-b;});
-                    console.log(visiblePages);      
-                    
+                    console.log(visiblePages);
+
                     // We now have the visible pages, so we want to prepare the
                     // clickable boxes on those pages
-                    
-                                      
-                        
 
-                };
-                
-                function _calculate_pointer_collision(boxes)
-                {
-                    // Store a list of x
-//                    var xCollisions = [];
-                    
+
+
+
                 }
-                
-                 
+
+//                function _calculate_pointer_collision(boxes)
+//                {
+//                    // Store a list of x
+////                    var xCollisions = [];
+//
+//                }
+
+
                 /*
                  * Public functions
                  */
-                
+
+                divaInstance.createAnnotation = function()
+                {
+                    annotationObj.push(new Annotation(0,0));
+                };
+
                 /**
                  * Bind a text annotation to an entire page.
-                 * 
+                 *
                  * @param {type} pageIdx
                  * @param {type} textContent
                  * @returns {undefined}
@@ -133,7 +286,7 @@ Allows you to highlight regions of a page image
                     // Save it
                     annotationObj.page[pageIdx] = String(textContent);
                 };
-                     
+
                 /**
                  * Flushes all of the Page Annotations
                  */
@@ -142,28 +295,33 @@ Allows you to highlight regions of a page image
                     // Clear out the page annotations
                     annotationObj.page = {};
                 };
-                
-                divaInstance.annotateAreaOnPage = function(pageIdx, textContent, regions, colour, divClass)
-                {                 
+
+                divaInstance.annotateAreaOnPage = function(pageIdx, textContent)
+                {
                     // Create the corresponding highlight box
-                    divaInstance.highlightOnPage(pageIdx, regions, colour, divClass);
-                    
+//                    divaInstance.highlightOnPage(pageIdx, regions, colour, divClass);
+
                     // Prepare for the annotation
                     if (annotationObj.area[pageIdx] === undefined)
                     {
                        // No annotation has been added to this page yet
                        annotationObj.area[pageIdx] = [];
                     }
-                    // Add it to the list 
+                    // Add it to the list
                     annotationObj.area[pageIdx].push(String(textContent));
                 };
-                        
+
                 divaInstance.getAnnotations = function()
                 {
                    return annotationObj;
                 };
-                
+
                 return true;
+            },
+            handleClick: function()
+            {
+                $(".diva-annotate-window").show();
+                return false;
             },
             pluginName: 'annotate',
             titleText: 'Annotate text on page'
